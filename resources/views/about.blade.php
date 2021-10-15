fc2637fbf47cc03a2951a4f63be87f7005cc5461
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2021-10-15 15:30:55 +0700

    membuat model untuk mengelola data di database dan membuat controller untuk mengarahkannya ke method di kelas PostController.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
     <h1>Halaman About</h1>
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="500">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="500" class="img-thumbnaill rounded">
 @endsection
