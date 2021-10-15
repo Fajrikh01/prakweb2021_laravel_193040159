a2559371c02e121a7c62726bb506b3ff6d8a2ec5
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2021-10-15 20:54:15 +0700

    melakukan penambahan data ke database menggunakan blade escape, melakukan pengubahan data dan melakukan query sesuai data yang di cari

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Fajri Khoirunnisa",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae atque officia voluptates harum ratione quae dolorum ipsa doloribus, officiis placeat, error sit odit mollitia, molestias ullam labore! Hic eum tempore reiciendis incidunt, magnam recusandae necessitatibus nesciunt et nostrum expedita totam qui ea eaque sequi id veritatis quidem, nisi eligendi, debitis ducimus dignissimos? Fuga in ipsa placeat aperiam, quam nemo rerum? Quos ipsum minus sequi distinctio quae, nam esse laudantium error impedit blanditiis reprehenderit quis suscipit molestias veritatis omnis! Assumenda et voluptatem temporibus sint modi nemo quae repellat, quia laboriosam perspiciatis dolor impedit cupiditate doloribus saepe unde rem ex tempora inventore!"
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Park Woojin",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis suscipit quia maiores vel sit sint, ad voluptatibus. Soluta amet repellat rerum optio vero, accusantium ratione fuga nemo quia odio ipsa a laborum dolorum alias iusto assumenda maxime quaerat quo delectus neque distinctio. Eaque, quos voluptates ipsum error illum consequatur, quia accusantium repellendus ea, magni illo sint. Nesciunt, id, nam necessitatibus architecto placeat quo soluta alias maxime et iure molestiae suscipit. Modi illo aperiam doloribus est error provident magnam similique?"
-        ],
-    ];
+    use HasFactory;
 
-    public static function all() {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug) {
-        $posts = static::all();
-        
-        // $post = [];
-        // foreach($posts as $p) {
-        //     if($p["slug"] === $slug) {
-        //         $post = $p;
-        //     }
-        // }
-        return $posts->firstwhere('slug', $slug);
-    }
+    //protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
