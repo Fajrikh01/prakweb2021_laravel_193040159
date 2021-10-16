bb661b03bbf130473dc1ced6d932b69d5f00c727
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2021-10-16 14:49:06 +0700

    belajar relationship menggunakan eloquent, relasi one to many dan one to one

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,8 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>{{ $post->author }}</h5>
+
+        <h5>By. Fajri Khoirunnisa in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
         
         {!! $post->body !!}
     </article>
