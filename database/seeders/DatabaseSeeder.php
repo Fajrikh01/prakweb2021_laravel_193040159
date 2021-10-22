a53650c88510077b0037516b1abe6d0c4e827a98
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2021-10-22 15:03:29 +0700

    membuat data secara random menggunakan factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
+
+        /*
         User:: create([
             'name' => 'Fajri Khoirunnisa',
             'email' => 'fajrikhoi@gmail.com',
@@ -29,6 +30,9 @@ class DatabaseSeeder extends Seeder
             'email' => 'ujin@gmail.com',
             'password' => bcrypt('12345')
         ]);
+        */
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programing',
@@ -40,6 +44,9 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
+        Post::factory(20)->create();
+
+        /*
         Post::create([
             'title' => 'Judul Pertama',
             'slug' => 'judul-pertama',
@@ -74,6 +81,6 @@ class DatabaseSeeder extends Seeder
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis debitis sequi nobis cupiditate, labore pariatur totam culpa magnam, deserunt, in quidem eaque expedita reiciendis! Corporis assumenda labore numquam, doloribus molestiae fugiat quibusdam sit veritatis ipsum tempore cum, dicta atque, quo sint nobis perferendis! Necessitatibus beatae, sapiente blanditiis commodi provident nulla repellendus molestias tempora earum repellat minima! Ut recusandae deleniti fuga accusantium unde deserunt nisi at, laudantium placeat, rerum magnam nihil aut optio molestiae. Qui, odit laboriosam, sed quaerat delectus similique fugiat consequatur alias earum, minus debitis repellat voluptates sapiente? Accusantium officiis eius commodi atque laudantium repudiandae labore dolor maiores libero.',
             'category_id' => 2,
             'user_id' => 2
-        ]);
+        ]);*/
     }
 }
